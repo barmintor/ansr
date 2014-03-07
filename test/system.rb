48b58aec3e1df9fc01b6567c243f57d9407ccff9
Benjamin Armintor <armintor@gmail.com>
2014-03-07 15:22:50 -0500

    make some documentary comments in the system test script

--- test/system.rb
+++ test/system.rb
@@ -2,27 +2,38 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app/models'))
 require 'adpla'
 require 'item'
 
+# you config the model with a hash including an API key for dp.la/v2, or the path to a YAML file
 Item.config('config/dpla.yml')
 
+# then you can find single items with known IDs
 puts Item.find("7eb617e559007e1ad6d95bd30a30b16b")
 
-opts = {q: 'kittens', facets: 'sourceResource.contributor'}
-# what blows up when we pass the facets and fields in as args?
-puts Item.api.items_path(opts)
-rel = Item.where(opts)
-rel.to_a
-rel = Item.where(q: 'kittens').limit(2).filter('sourceResource.contributor') #.select('sourceResource.title').select('object.originalRecord')
-rel.to_a.each do |item|
-  puts "#{item["id"]} \"#{item['sourceResource.title']}\" \"#{item['originalRecord']}\""
-end
 
+# or you can search with ActiveRecord::Relations
+opts = {q: 'kittens', facets: 'sourceResource.contributor'}
+rel = Item.where(opts)
+# this means the results are lazy-loaded, #load or #to_a will load them
+rel.to_a
+# you can also assemble the queries piecemeal with the Relation's decorator pattern
+# the where decorator adds query fields
+# the select decorator adds response fields
+# the limit decorator adds a page size limit
+# the offset decorator adds a non-zero starting point in the response set
+# the filter decorator adds filter/facet fields and optionally values to query them on
+rel = Item.where(q: 'kittens').limit(2).filter('sourceResource.contributor').select('sourceResource.title')
+rel.to_a.each do |item|
+  puts "#{item["id"]} \"#{item['sourceResource.title']}\""
+end
+# the filter values for the query are available on the relation after it is loaded
 rel.filters.each do |k,f|
   puts "#{k} values"
   f.items.each do |item|
     puts "filter:  \"#{item.value}\" : #{item.hits}"
   end
 end
-
+# the loaded Relation has attributes describing the response set
+rel.count # the size of the response
+# the where decorator can be negated
 rel = rel.where.not(q: 'cats')
 
 rel.to_a.each do |item|
